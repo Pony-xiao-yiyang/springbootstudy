1784147b08718961b240b9eba72a24194a46067d
xiaoliyi <757330279@qq.com>
2020-10-10 17:53:35 +0800

    springbootday04学习

---
+++ springboot-study-day04-demo01/src/main/java/com/example/springbootstudyday04demo01/SpringbootStudyDay04Demo01Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudyday04demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyDay04Demo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyDay04Demo01Application.class, args);
+	}
+
+}
---
+++ springboot-study-day04-demo01/src/test/java/com/example/springbootstudyday04demo01/SpringbootStudyDay04Demo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudyday04demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyDay04Demo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

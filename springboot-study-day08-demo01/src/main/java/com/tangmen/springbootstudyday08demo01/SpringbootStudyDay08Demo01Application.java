add4bed951516433040a68c7861473b303789ba0
xiaoliyi <757330279@qq.com>
2020-11-17 15:49:29 +0800

    thymeleaf的使用

---
+++ springboot-study-day08-demo01/src/main/java/com/tangmen/springbootstudyday08demo01/SpringbootStudyDay08Demo01Application.java
@@ -0,0 +1,13 @@
+package com.tangmen.springbootstudyday08demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyDay08Demo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyDay08Demo01Application.class, args);
+	}
+
+}
---
+++ springboot-study-day08-demo01/src/test/java/com/tangmen/springbootstudyday08demo01/SpringbootStudyDay08Demo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangmen.springbootstudyday08demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyDay08Demo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ece3cb60465f5e59f32c0e5d8bf68d36bd63bc0f
xiaoliyi <757330279@qq.com>
2020-11-26 16:34:14 +0800

    springboot security 学习

---
+++ springboot-study-day13-security/src/main/java/com/tangmen/springbootstudyday13security/SpringbootStudyDay13SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.tangmen.springbootstudyday13security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyDay13SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyDay13SecurityApplication.class, args);
+	}
+
+}
---
+++ springboot-study-day13-security/src/test/java/com/tangmen/springbootstudyday13security/SpringbootStudyDay13SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangmen.springbootstudyday13security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyDay13SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

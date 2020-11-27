4ce77e4099400b41e4fd35c6029e4749e639a3de
xiaoliyi <757330279@qq.com>
2020-11-27 16:31:34 +0800

    springboot security 注销,权限控制以及记住我及定制页面

---
+++ springboot-study-day14-security/src/main/java/com/tangmen/springbootstudyday14security/SpringbootStudyDay14SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.tangmen.springbootstudyday14security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyDay14SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyDay14SecurityApplication.class, args);
+	}
+
+}
---
+++ springboot-study-day14-security/src/test/java/com/tangmen/springbootstudyday14security/SpringbootStudyDay14SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangmen.springbootstudyday14security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyDay14SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

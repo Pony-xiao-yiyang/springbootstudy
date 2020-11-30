dc4354ec1c7cd2a854ca05c9cbde240b7ef28422
肖力弋 <757330279@qq.com>
2020-11-30 23:59:01 +0800

    shiro 体验（文档学习+安装+github学习）

---
+++ springboot-study-day15-shiro-demo01/src/main/java/com/tangmen/springbootstudyday15shirodemo01/SpringbootStudyDay15ShiroDemo01Application.java
@@ -0,0 +1,13 @@
+package com.tangmen.springbootstudyday15shirodemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyDay15ShiroDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyDay15ShiroDemo01Application.class, args);
+	}
+
+}
---
+++ springboot-study-day15-shiro-demo01/src/test/java/com/tangmen/springbootstudyday15shirodemo01/SpringbootStudyDay15ShiroDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangmen.springbootstudyday15shirodemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyDay15ShiroDemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
